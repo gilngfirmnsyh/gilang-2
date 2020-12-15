4422049128867f1629d5755c148b55703af808e4
gilngfirmnsyh <76056079+gilngfirmnsyh@users.noreply.github.com>
2020-12-15 22:34:06 +0700

    Add files via upload

---
+++ data/titikacuan2_1.js
@@ -0,0 +1,8 @@
+var json_titikacuan2_1 = {
+"type": "FeatureCollection",
+"name": "titikacuan2_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.424842743277978, -6.99041715487735 ] } }
+]
+}
